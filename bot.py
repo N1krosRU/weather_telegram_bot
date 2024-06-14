737836bfbb7192a3f08df6dcc41cbcd5e997eacd
root <root@>
2024-06-15 01:27:59 +0300

    add gismeteo api

--- bot.py
+++ bot.py
@@ -3,26 +3,22 @@ import dotenv
 import os
 import telebot
 from telebot import types
-import weather
+import gismeteo
 
+# Считываем ключ из переменной окружения или файла .env если он имеется
 dotenv.load_dotenv()
-BOT_API_KEY = os.getenv("BOT_API_KEY")  # Считываем ключ из переменной окружения или файла .env если он имеется
+BOT_API_KEY = os.getenv("BOT_API_KEY")
 bot = telebot.TeleBot(BOT_API_KEY)
 
 
 # Обработчики команд
-@bot.message_handler(commands=['help'])
-def command_help(message):
-    bot.send_message(message.from_user.id, 'Нажимай на кнопки под областью чата и узнай погоду в любой точки мира')
-
-
 @bot.message_handler(commands=['start'])
 def command_start(message):
     keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
-    buttoon_gps = types.KeyboardButton(text='Узнать погоду по GPS', request_location=True)
-    buttoon_place = types.KeyboardButton('Узнать погоду в конкретном месте')
-    keyboard.add(buttoon_gps, buttoon_place)
-    bot.send_message(message.from_user.id, 'Нажимай на кнопки под областью чата и узнай погоду в любой точки мира',
+    buttoon_gps = types.KeyboardButton(
+        text='Узнать погоду по метке геопозиции', request_location=True)
+    keyboard.add(buttoon_gps)
+    bot.send_message(message.from_user.id, 'Нажимай на кнопку под областью чата и узнай погоду в любой точке мира',
                      reply_markup=keyboard)
 
 
@@ -30,18 +26,28 @@ def command_start(message):
 def location(message):
     if message.location is not None:
         coordinates = f'{message.location.latitude},{message.location.longitude}'
-        bot.send_message(message.from_user.id, weather.what_location(coordinates))
-        bot.send_message(message.from_user.id, weather.what_weather(coordinates))
+        bot.send_message(message.from_user.id,
+                         f"Ваши координаты: {coordinates}")
+        bot.send_message(message.from_user.id,
+                         gismeteo.get_cur_weather_by_geo(latitude=message.location.latitude, longitude=message.location.longitude))
+        # bot.send_message(message.from_user.id, weather.what_location(coordinates))
+        # bot.send_message(message.from_user.id, weather.what_weather(coordinates))
 
 
 @bot.message_handler(content_types=["text"])
-def weather_place(message):
-    if message.text == 'Узнать погоду в конкретном месте':
-        bot.send_message(message.from_user.id, 'Где узнать погоду?')
-    else:
-        user_place = message.text
-        bot.send_message(message.from_user.id, weather.what_location(user_place))
-        bot.send_message(message.from_user.id, weather.what_weather(user_place))
+def print_message(message):
+    bot.send_message(message.from_user.id,
+                     f"Привет {message.from_user.username}, отправь мне свою геопозицию и узнай погоду")
+
+# def weather_place(message):
+#     if message.text == 'Узнать погоду в конкретном месте':
+#         bot.send_message(message.from_user.id, 'Где узнать погоду?')
+#     else:
+#         user_place = message.text
+#         bot.send_message(message.from_user.id,
+#                          weather.what_location(user_place))
+#         bot.send_message(message.from_user.id,
+#                          weather.what_weather(user_place))
 
 
 bot.polling(none_stop=True, interval=0)
---
+++ gismeteo.py
@@ -0,0 +1,25 @@
+import dotenv
+import os
+import requests
+
+# Считываем ключ из переменной окружения или файла .env если он имеется
+dotenv.load_dotenv()
+GISMETEO_TOKEN = os.getenv("GISMETEO_TOKEN")
+
+
+def get_cur_weather_by_geo(latitude, longitude):
+    headers = {"X-Gismeteo-Token": GISMETEO_TOKEN,
+               "Accept-Encoding": "gzip, deflate"}
+    params = {"latitude": latitude, "longitude": longitude}
+    response = requests.get(
+        "https://api.gismeteo.net/v2/weather/current/", params=params, headers=headers)
+    # трансформируем ответ в виде json в словарь python'a
+    response_dict = response.json()
+    response_date = response_dict["response"]["date"]["local"]
+    response_description = response_dict["response"]["description"]["full"]
+    response_temp_air = response_dict["response"]["temperature"]["air"]["C"]
+    response_temp_comfort = response_dict["response"]["temperature"]["comfort"]["C"]
+    return (f"""Сейчас на улице {response_description}
+    Температура {response_temp_air}
+    Ощущается как {response_temp_comfort},
+    Данные акутальны на момент {response_date}""")
